17648bd99b0b84f0aa7f7f34f81bd381b0d66daf
mysha <maimunarahman30@gmail.com>
2024-12-08 05:02:07 +0600

    search added in addMovies ROute

--- index.js
+++ index.js
@@ -31,17 +31,21 @@ async function run() {
    
     const database=client.db('MovieDB');
     const Added=database.collection('addMovies');
+    const wishList=client.db('wish').collection('favourite')
 
 
 
-
-     
-    app.get('/addMovies',async(req,res)=>{
-      const cursor=Added.find();
-      const result= await cursor.toArray();
-      res.send(result);
-
-    })
+    app.get('/addMovies', async (req, res) => {
+      const { title } = req.query;
+    
+        const query = title
+          ? { title: { $regex: title, $options: "i" } } 
+          : {};
+        const movies = await Added.find(query).toArray(); 
+        console.log("Query:", query);
+   res.json(movies);
+      
+    });
     app.get('/featured',async(req,res)=>{
       const cursor=Added.find();
       const result = await Added.find().sort({ rating: -1 }).limit(6).toArray();
@@ -59,12 +63,12 @@ async function run() {
       
         const result=await Added.insertOne(movie);
         res.send(result)
-     
-
-      
-
+ 
     })
-
+    // await Added.createIndex({ title: "text", genre: "text" });
+   
+    
+    
 
     
     app.get('/details/:id', async(req,res)=>{
@@ -93,7 +97,7 @@ async function run() {
         };
         const options = { upsert: true };
     
-        const result = await Added.updateOne(query, update, options);
+        const result = await wishList.updateOne(query, update, options);
         res.send(result);
     
     });
@@ -101,12 +105,49 @@ async function run() {
       const email = req.params.email;
     
         const query = { email: email }; 
-        const userFavorites = await Added.findOne(query);    
+        const userFavorites = await wishList.findOne(query);    
         res.send(userFavorites.movies);
     
     });
     
-    
+    app.delete('/favourites/:email', async(req,res)=>{
+      const email=req.params.email;
+      console.log('email deleted successfully')
+      const query={email: email}
+      const result =await wishList.deleteOne(query)
+      res.send(result)
+    })
+    app.get('/update/:id', async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)}
+      const movie=await Added.findOne(query)
+      console.log('update',query);
+      res.send(movie)
+ })
+
+ app.put('/update/:id', async(req, res)=>{
+
+     const id=req.params.id;
+     const movie=req.body;
+     console.log('update',movie);
+     const filter={_id : new ObjectId(id)}
+     const option={upsert:true}
+     const updateUser={
+         $set:{
+          poster:movie.poster,
+             title: movie.title,
+             poster:movie.poster,
+            rating:movie.rating,
+           overview:movie.overview,
+
+
+
+         }
+     }
+     const result= await Added.updateOne(filter,updateUser,option)
+     res.send(result)
+
+ })
    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
