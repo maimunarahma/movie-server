f979f9c84b489c12999156bc252c68b4709129aa
mysha <maimunarahman30@gmail.com>
2024-12-08 21:49:42 +0600

    final

--- index.js
+++ index.js
@@ -49,9 +49,6 @@ async function run() {
     app.get('/featured',async(req,res)=>{
       const cursor=Added.find();
       const result = await Added.find().sort({ rating: -1 }).limit(6).toArray();
-      // Sort descending (highest values first)
-      // Limit to top 3 results
-    
       res.send(result);
 
     })
@@ -86,68 +83,82 @@ async function run() {
       res.send(result)
     })
 
-    app.post('/favourites/:email', async (req, res) => {
-      const email = req.params.email;
-      const movie = req.body; 
-    console.log('favoirite',email, movie)
+   // Add a movie to favourites
+app.post('/favourites/:email', async (req, res) => {
+  const email = req.params.email;
+  const movie = req.body;
 
-        const query = { email: email };
-        const update = {
-          $addToSet: { movies: movie },
-        };
-        const options = { upsert: true };
+  if (!movie || !movie._id) {
+    return res.status(400).send({ message: "Invalid movie data." });
+  }
+
+  const query = { email: email };
+  const update = {
+    $addToSet: { movies: movie }, // Prevent duplicate entries
+  };
+  const options = { upsert: true }; // Create document if it doesn't exist
+
+  const result = await wishList.updateOne(query, update, options);
+  res.send({ message: "Movie added to favourites.", result });
+});
+
+// Get all favourites for a user
+app.get('/favourites/:email', async (req, res) => {
+  const email = req.params.email;
+
+  const query = { email: email };
+  const userFavorites = await wishList.findOne(query);
+
+  if (!userFavorites || !userFavorites.movies || userFavorites.movies.length === 0) {
+    return res.status(404).send({ message: "No favourites found for this user." });
+  }
+
+  res.send({ message: "Favourites retrieved successfully.", movies: userFavorites.movies });
+});
+
+// Delete a movie from favourites
+app.delete('/favourites/:email/:id', async (req, res) => {
+  const { email, id } = req.params;
+  const query = { email: email };
+  const update = {
+    $pull: { movies: { _id: id } }, // Remove the movie with the matching _id
+  };
+
+  const result = await wishList.updateOne(query, update);
+
+  if (result.modifiedCount > 0) {
+    res.send({ message: "Movie removed from favourites successfully!" });
+  } else {
+    res.status(404).send({ message: "Movie not found in favourites for this user." });
+  }
+});
+
+
+    app.put('/update/:id', async (req, res) => {
     
-        const result = await wishList.updateOne(query, update, options);
-        res.send(result);
-    
-    });
-    app.get('/favourites/:email', async (req, res) => {
-      const email = req.params.email;
-    
-        const query = { email: email }; 
-        const userFavorites = await wishList.findOne(query);    
-        res.send(userFavorites.movies);
-    
-    });
-    
-    app.delete('/favourites/:email', async(req,res)=>{
-      const email=req.params.email;
-      console.log('email deleted successfully')
-      const query={email: email}
-      const result =await wishList.deleteOne(query)
-      res.send(result)
-    })
-    app.get('/update/:id', async(req,res)=>{
-      const id=req.params.id;
-      const query={_id: new ObjectId(id)}
-      const movie=await Added.findOne(query)
-      console.log('update',query);
-      res.send(movie)
- })
-
- app.put('/update/:id', async(req, res)=>{
-
-     const id=req.params.id;
-     const movie=req.body;
-     console.log('update',movie);
-     const filter={_id : new ObjectId(id)}
-     const option={upsert:true}
-     const updateUser={
-         $set:{
-          poster:movie.poster,
-             title: movie.title,
-             poster:movie.poster,
-            rating:movie.rating,
-           overview:movie.overview,
-
-
-
-         }
-     }
-     const result= await Added.updateOne(filter,updateUser,option)
-     res.send(result)
-
- })
+          const id = req.params.id;
+          const movie = req.body;
+  
+          console.log("Received ID:", id);
+          console.log("Received Data:", movie);
+  
+          const filter = { _id: new ObjectId(id) };
+          const option = { upsert: true };
+          const updateUser = {
+              $set: {
+                  poster: movie.poster,
+                  title: movie.title,
+                  rating: movie.rating,
+                  overview: movie.overview,
+              },
+          };
+  
+          const result = await Added.updateOne(filter, updateUser, option);
+          console.log("MongoDB Update Result:", result);
+          res.send(result);
+   
+  });
+  
    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
